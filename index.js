6a6b9301d48df6f7d19cdfc8162f309cb9b92f9a
Chip Lee <chip.lee@petuum.com>
2022-02-08 15:54:40 -0500

    adjust zoom

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function initMap() {
     const uluru = { lat: 42.66686, lng: -82.63673 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 12,
       center: uluru,
     });
     // The marker, positioned at Uluru
